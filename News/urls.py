e787695b8f1208be782deee671e90d5c48fbafae
Burim98 <burimka98@gmail.com>
2020-04-14 13:14:49 +0300

    Рубрики и Новости №1
    Добавил страницу с новостями, сортировку по рубрикам и связал все ссылками

--- News/urls.py
+++ News/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, new, by_rubric
 
 urlpatterns = [
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', index),
+    path('new', new, name='NewsIndex'),
 ]
--- News_Site/urls.py
+++ News_Site/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('News.urls')),
+    path('new', include('News.urls')),
     path('admin/', admin.site.urls),
 ]
